c08d8de377132f0e6b3d551aff9c13a99ba61535
Fathulx <124646754+Fathulx@users.noreply.github.com>
2026-06-11 13:55:40 +0700

    Tambah form input nama dan alamat

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Form</title>
+</head>
+<body>
+    <form action="proses.php" method="get">
+        <label>Nama</label><br>
+        <input name="nama" type="text" placeholder="Masukkan nama"><br>
+        <label>Alamat</label><br>
+        <input name="alamat" type="text" placeholder="Masukkan alamat"><br>
+        <button type="submit">Submit</button>
+    </form>
+</body>
+</html>
